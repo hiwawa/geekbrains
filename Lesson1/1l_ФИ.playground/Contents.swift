f4c4d06febec6a62658d956c6409096180b1c594
Aleksander Pankow <aleksander.pankow@icloud.com>
2020-08-27 22:46:42 +0200

    Домашка

--- Lesson1/1l_ФИ.playground/Contents.swift
+++ Lesson1/1l_ФИ.playground/Contents.swift
@@ -1,21 +1,48 @@
 //: A UIKit based Playground for presenting user interface
   
 import UIKit
-import PlaygroundSupport
 
-class MyViewController : UIViewController {
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
+//Задание 1: уравнениe вида ax2+bx+c=0
 
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
-        self.view = view
-    }
-}
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+let a: Int = 5
+let b: Int = 10
+let c: Int = 2
+print("Уравнениe вида ax2+bx+c=0. Значения переменных: a = 5, b = 4, c = 2")
+var D: Int = (b*b) - (4*a*c)
+print("Дискриминант равен: " + String(D))
+var korenD = sqrt(Double(D))
+
+if D<0{
+    print("Нет корней")
+};
+if (D>0) {
+    let x1 = (Double(-b) + korenD)/(2*Double(a))
+    let x2 = (Double(-b) - korenD)/(2*Double(a))
+    print("x1 равен: " + String(format: "%.2f", x1)
+        + ", x2 равен: " + String(format: "%.2f", x2))
+};
+if (D==0) {
+    let x = Int(-b)/(2*Int(a))
+    print("Переменная х равна " + String(x))
+};
+
+//Задание 2: Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
+
+let katet1: UInt = 5 //Только положительное значение
+let katet2: UInt = 3 //Только положительное значение
+print("Даны катеты треугольника: " + String(katet1) + " и " + String(katet2))
+var S: Double = 0.5 * Double(katet1) * Double(katet2)
+print("Площадь треугольника равна " + String(S))
+var G: Double = sqrt((Double(katet1)*Double(katet1)) + (Double(katet2)*Double(katet2)))
+print("Гипотенуза треугольника равна " + String(format: "%.2f",G))
+var P: Double = Double(katet1) + Double(katet2) + G
+print("Периметр треугольника равен " + String(format: "%.2f",P))
+
+//Задание 3: Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет
+
+let summa : UInt = 5000 //Мы ложим положительный баланс UInt :)
+let procent: Double = 3.6 //3.6% годовых
+let srok : Int = 5
+let profit: Double = ((Double(summa) * procent * Double(srok*365))/(365*100))
+let summavozvrata: Double = (Double(summa) + profit)
+print("Наш профит за " + String(srok) + " лет равен: " + String(profit) + ", поэтому общая сумма вклада через 5 лет будет равна " + String(summavozvrata))
