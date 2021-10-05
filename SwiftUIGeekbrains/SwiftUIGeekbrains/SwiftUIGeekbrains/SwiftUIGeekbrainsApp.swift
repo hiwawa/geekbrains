2368b8c7f4d19eb7112294cc4899196a4b50be05
Aleksander Pankow <aleksander.pankow@icloud.com>
2021-10-05 10:31:58 +0200

    SWIFT UI HomeWork Lesson 1

---
+++ SwiftUIGeekbrains/SwiftUIGeekbrains/SwiftUIGeekbrains/SwiftUIGeekbrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGeekbrainsApp.swift
+//  SwiftUIGeekbrains
+//
+//  Created by Aleksander Pankow on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGeekbrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
