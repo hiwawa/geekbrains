01e4f4bb123494ec304f3852cde09a67888832b6
Aleksander Pankow <aleksander.pankow@icloud.com>
2022-03-26 09:11:18 +0100

    NewVKAppV2

---
+++ VKSwiftUI/VKSwiftUI/Models/Friend.swift
@@ -0,0 +1,15 @@
+//
+//  Friend.swift
+//  VKSwiftUI
+//
+//  Created by Aleksander Pankow on 26/03/2022.
+//
+
+import Foundation
+
+struct Friend: Identifiable{
+    var id: Int
+    var firstName: String
+    var lastName: String
+    var online: Bool
+}
---
+++ VKSwiftUI/VKSwiftUI/VKSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKSwiftUIApp.swift
+//  VKSwiftUI
+//
+//  Created by Aleksander Pankow on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct VKSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginVK()
+        }
+    }
+}
